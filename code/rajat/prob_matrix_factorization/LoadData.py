ebefa9d70ef95352a56bf2fe590af7152b201607
Rajat <rajat@gmail.com>
2021-04-29 12:08:29 +0200

    Add code for pmf

---
+++ code/rajat/prob_matrix_factorization/LoadData.py
@@ -0,0 +1,44 @@
+from numpy import *
+import random
+import pandas as pd
+import numpy as np
+
+def load_cf_data(file_path = '../../data/data_train.csv'):
+    data_pd = pd.read_csv(file_path)
+    users, movies = \
+        [np.squeeze(arr) for arr in
+         np.split(data_pd.Id.str.extract('r(\d+)_c(\d+)').values.astype(int) - 1, 2, axis=-1)]
+    predictions = data_pd.Prediction.values
+    data = np.vstack((users, movies, predictions))
+    data_list = data.tolist()
+    return data_list
+
+def load_rating_data(file_path='ml-100k/u.data'):
+    """
+    load movie lens 100k ratings from original rating file.
+    need to download and put rating data in /data folder first.
+    Source: http://www.grouplens.org/
+    """
+    prefer = []
+    for line in open(file_path, 'r'):
+        (userid, movieid, rating, ts) = line.split('\t')
+        uid = int(userid)
+        mid = int(movieid)
+        rat = float(rating)
+        prefer.append([uid, mid, rat])
+    data = array(prefer)
+    return data
+
+
+def spilt_rating_dat(data, size=0.2):
+    train_data = []
+    test_data = []
+    for line in data:
+        rand = random.random()
+        if rand < size:
+            test_data.append(line)
+        else:
+            train_data.append(line)
+    train_data = array(train_data)
+    test_data = array(test_data)
+    return train_data, test_data
---
+++ code/rajat/prob_matrix_factorization/README.md
@@ -0,0 +1,16 @@
+## Python Implementation of Probabilistic Matrix Factorization Algorithm 
+
+The code attempts to implement the following paper:
+
+> *Mnih, A., & Salakhutdinov, R. (2007). Probabilistic matrix factorization. In Advances in neural information processing systems (pp. 1257-1264).*
+
+Probabilistic Matrix Factorization in Python with MovieLens dataset
+
+![image](https://github.com/fuhailin/Probabilistic-Matrix-Factorization/blob/master/docs/PMF.png)
+
+The dataset is a copy of the MovieLens | GroupLens 
+dataset in the `MovieLens 100k | GroupLens <http://files.grouplens.org/datasets/movielens/ml-100k.zip/>`
+
+### Reference:  
+1. Mnih, A., & Salakhutdinov, R. (2007). Probabilistic matrix factorization. In Advances in neural information processing systems (pp. 1257-1264).  
+2. Salakhutdinov, R. Probabilistic matrix factorization in Matlab. http://www.utstat.toronto.edu/~rsalakhu/BPMF.html.  
